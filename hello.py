3480f5ba1d9d5602233e376f7432b02e1bf51bfb
SnehaKK <kulsneha04@gmail.com>
2014-11-24 22:32:42 -0800

    Render Template

--- hello.py
+++ hello.py
@@ -1,8 +1,12 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+	return render_template('landing-page.html')
+    # return 'Hello World!'
+
+if __name__ == '__main__':
+	app.run(host="0.0.0.0",port=5000,debug=True)
